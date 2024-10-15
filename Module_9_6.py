29f2a0f8bcc041324716ad987aeb2b2ba0da040e
Mavridiy <161286685+Mavridiy@users.noreply.github.com>
2024-10-15 22:00:02 +0300

    Add files via upload

---
+++ Module_9_6.py
@@ -0,0 +1,11 @@
+
+def all_variants(text):
+    n = len(text)
+    for length in range(1, n + 1):
+        for i in range(n - length + 1):
+            yield text[i:i + length]
+
+# Пример работы функции:
+a = all_variants("abc")
+for i in a:
+    print(i)
